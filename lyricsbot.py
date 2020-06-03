4386db8cfb2bc3ae698b6cc87a4d4a1eb6144134
tvlb <tessavioletlyricsbot@gmail.com>
2020-06-03 16:23:58 +0100

    added error handling, removed dm idea

--- lyricsbot.py
+++ lyricsbot.py
@@ -16,19 +16,11 @@ lyrics=lyricfile.read()
 lyrics = lyrics.split("\n")
 
 x = 1
-while x < 2:
-    y = random.randint(0,len(lyrics))
-    print(lyrics[y])
-    api.update_status(lyrics[y])
-    time.sleep(1800)
-
-new_followers = API.followers(user)
-
-for i in new_followers:
-    newDM = raw_input (i.from_user + "send follower DM?" + "Y/N" )
-    if newDM.lower() == "n":
-        print i.from_user + " was not messaged"
-        print "Now returning to the Main Menu."
-else:
-    api.send_direct_message(user_id = i.from_user, text = "Hi there! I'm a new bot on twitter and I was wondering if you'd take the time to share my account around! Maybe that would make the 4 hours of code worth it :)")
-    print "You messaged " + i.from_user
+try:
+    while x < 2:
+        y = random.randint(0,len(lyrics))
+        print(lyrics[y])
+        api.update_status(lyrics[y])
+        time.sleep(5)
+except:
+    pass
