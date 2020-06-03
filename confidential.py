b9322c592143b567369df3e44eb73bc07d1778bb
tvlb <tessavioletlyricsbot@gmail.com>
2020-06-03 13:13:27 +0100

    lyricsbot.py

---
+++ confidential.py
@@ -0,0 +1,6 @@
+
+
+consumer_key = 'TDzEfcM7thNcDUloJTnWHyE8N'
+consumer_secret = 'G5O02oFMWxXnU6u0bEE42omZngPH6jHqCpmZ11jMYeFPmyvBbt' 
+access_token = '1268005692188868610-sXF1lyWV2GJrUVxKa1JzzSFVXLlrCO' 
+access_token_secret = 'E8n3se4oDzVp01LASmKiAPZxcU6Q1wkKHGb1zlZ1bOuHK' 
---
+++ lyricsbot.py
@@ -0,0 +1,23 @@
+
+'''tessa violet lyrics bot'''
+
+import time
+import random
+import tweepy
+
+from confidential import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+lyricfile = open("lyrics.txt", "r")
+lyrics=lyricfile.read()
+lyrics = lyrics.split("\n")
+
+x = 1
+while x < 2:
+    y = random.randint(0,len(lyrics))
+    print(lyrics[y])
+    api.update_status(lyrics[y])
+    time.sleep(1800)
