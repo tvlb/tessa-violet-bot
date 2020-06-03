1cf321ecec901025c17412e10664bbb905a639f3
tvlb <66366971+tvlb@users.noreply.github.com>
2020-06-03 13:54:52 +0100

    Update lyricsbot.py

--- lyricsbot.py
+++ lyricsbot.py
@@ -21,3 +21,14 @@ while x < 2:
     print(lyrics[y])
     api.update_status(lyrics[y])
     time.sleep(1800)
+
+    new_followers = API.followers(user)
+
+for i in new_followers:
+    newDM = raw_input (i.from_user + "send follower DM?" + "Y/N" )
+    if newDM.lower() == "n":
+        print i.from_user + " was not messaged"
+        print "Now returning to the Main Menu."
+else:
+    api.send_direct_message(user_id = i.from_user, text = "message text here")
+    print "You messaged " + i.from_user
